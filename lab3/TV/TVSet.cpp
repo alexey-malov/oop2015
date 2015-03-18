f975b90dab8818cf58f33670bb1c2c9e2536b960
Alexey Malov <alexey.malov@cpslabs.net>
2015-03-18 08:44:17 +0300

    Revert "Revert "Добавлен каркас класса tvset""
    This reverts commit 11cd432f2d9b6b4b3dde5330dc07faf633195cf4.

---
+++ lab3/TV/TVSet.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "TVSet.h"
+
+
+CTVSet::CTVSet()
+{
+}
+
+
+CTVSet::~CTVSet()
+{
+}
---
+++ lab3/TV/TVSet.h
@@ -0,0 +1,8 @@
+#pragma once
+class CTVSet
+{
+public:
+	CTVSet();
+	~CTVSet();
+};
+
